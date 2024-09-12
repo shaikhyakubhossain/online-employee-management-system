dd2955b8e87cf6ff3de0a54dccdfec3d651a469c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-12 13:25:38 +0530

    Add MessageBubble component

---
+++ client/online-employee-management-system/src/components/MessageBubble/message-bubble.component.tsx
@@ -0,0 +1,33 @@
+import styles from "./message-bubble.module.scss";
+
+type propsType = {
+  message: string;
+  sentBy: string | undefined;
+  customTW?: string;
+};
+
+export default function MessageBubble(props: propsType): JSX.Element {
+  let toggleColor;
+  let toggleSide;
+
+  if (props.sentBy === "you") {
+    toggleColor = "bg-green-500";
+    toggleSide = "justify-end";
+  } else {
+    toggleColor = "bg-white";
+    toggleSide = "justify-start";
+  }
+
+  return (
+    <div
+      className={`${styles.mainContainer} flex ${toggleSide} ${props.customTW}  text-left`}
+    >
+      <div
+        className={`text-slate-800 ${toggleColor} rounded my-2 px-4 py-3 overflow-x-auto break-words`}
+      >
+        <div className={`text-sm text-left`}>{props.sentBy}</div>
+        <div className={`text-2xl`}>{props.message}</div>
+      </div>
+    </div>
+  );
+}
