c8acf2d27644e26b9881a9f8b2c2a6c3ad67f254
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-12 22:47:39 +0530

    Update Button, and MessageInput components

--- client/online-employee-management-system/src/components/ChatAppMini/MessageInput/message-input.component.tsx
+++ client/online-employee-management-system/src/components/ChatAppMini/MessageInput/message-input.component.tsx
@@ -9,7 +9,7 @@ export default function MessageInput(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer} flex w-4/5 mx-auto`}>
             <input type="text" className={`${styles.textField} bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="message" />
-            <div className=''><Button onClick={props.sendMessageFunction} customTW='bg-gray-50 dark:bg-gray-700 hover:bg-pink-800 px-4 py-2 ml-2 mt-2'><div className=''>Send</div></Button></div>
+            <div className='flex items-center'><Button onClick={props.sendMessageFunction} customTW='bg-gray-50 dark:bg-gray-700 hover:bg-pink-800 px-4 py-2 ml-2 '>Send</Button></div>
         </div>
     )
 }
