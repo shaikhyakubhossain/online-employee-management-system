978a7c20e4e7758a538872c8b14054b920045e51
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-12 22:10:51 +0530

    Add constants

---
+++ client/online-employee-management-system/src/constants/Dashboard/data.ts
@@ -0,0 +1,69 @@
+type btnListType = {
+    name: string;
+    base64Icon: string;
+    detail: string;
+    peopleAddedList: string[];
+};
+
+export const btnList: btnListType[] = [
+    {
+        name: "Dashboard",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "",
+        peopleAddedList: []
+    },
+    {
+        name: "Notice",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "View all of your notices",
+        peopleAddedList: []
+    },
+    {
+        name: "Apply for leave",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Here you can apply for your next leave",
+        peopleAddedList: []
+    },
+    {
+        name: "Access/Asset",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Manage your all access and assets",
+        peopleAddedList: []
+    },
+    {
+        name: "Salary Slip",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Maintain your salary slip",
+        peopleAddedList: []
+    },
+    {
+        name: "Attendance",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        peopleAddedList: []
+    },
+    {
+        name: "Holiday",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        peopleAddedList: []
+    },
+    {
+        name: "Employee detail",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        peopleAddedList: []
+    },
+    {
+        name: "Calender",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        peopleAddedList: []
+    },
+    {
+        name: "Resign",
+        base64Icon: "M12 6v6m0 0v6m0-6h6m-6 0H6",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        peopleAddedList: []
+    },
+];
