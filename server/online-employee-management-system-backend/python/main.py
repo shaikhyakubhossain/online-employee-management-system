343e17765cc4ee1b865d84fffe9a52861d57ecb9
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-12 14:49:03 +0530

    Update main.py

--- server/online-employee-management-system-backend/python/main.py
+++ server/online-employee-management-system-backend/python/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Define a simple root endpoint
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to FastAPI"}
+    return {"message": "hi"}
 
 # Define an endpoint that returns an item based on its ID
 @app.get("/items/{item_id}")
