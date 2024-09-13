a5b7c9bf77b99af3aed2070c72902c01ada1f226
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-13 17:52:20 +0530

    Add Chat page

---
+++ client/online-employee-management-system/src/app/(pages)/Chat/page.tsx
@@ -0,0 +1,9 @@
+import RealTimeChat from "@/components/ChatAppMini/RealTimeChat/real-time-chat.component";
+
+export default function Chat() {
+  return (
+    <div className="">
+      <RealTimeChat />
+    </div>
+  )
+}
