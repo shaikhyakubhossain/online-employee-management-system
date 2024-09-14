1300d2675395cf37988717c6d1ce714ee9c73cc3
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-14 16:53:11 +0530

    Add svg.ts in constants, update HeroHeader component

--- client/online-employee-management-system/src/components/Dashboard/HeroHeader/hero-header.component.tsx
+++ client/online-employee-management-system/src/components/Dashboard/HeroHeader/hero-header.component.tsx
@@ -1,16 +1,17 @@
-import styles from './hero-header.module.scss'
+import styles from './hero-header.module.scss';
+import { sveForHeroHeader } from '@/constants/Svg/svg';
 
 export default function HeroHeader(): JSX.Element {
     return (
-        <div className={`${styles.mainContainer} flex justify-between min-h-36 max-h-80 mb-7 px-8 rounded-2xl`}>
+        <div className={`${styles.mainContainer} flex justify-between min-h-36 max-h-80 mb-7 pl-8 pr-24 rounded-2xl`}>
             <div className={`${styles.left} my-auto`}>
             <div className={`${styles.greeting} font-bold text-4xl`}>Hello User 👋</div>
             <div className='text-2xl'>You can manage your things from here</div>
             </div>
-            <div className={`${styles.right} flex my-auto`}>
-                <div></div>
-                <div className='font-bold text-6xl'>Welcome</div>
-                <div></div>
+            <div className={`${styles.right} flex my-auto relative`}>
+                <div className={`${styles.svgs} -left-24 -top-14`}><img src={sveForHeroHeader[0].base64} alt="" /></div>
+                <div className='font-bold text-6xl my-auto'>Welcome</div>
+                <div className={`${styles.svgs} -right-16 -top-11`}><img src={sveForHeroHeader[1].base64} alt="" /></div>
             </div>
         </div>
     )
