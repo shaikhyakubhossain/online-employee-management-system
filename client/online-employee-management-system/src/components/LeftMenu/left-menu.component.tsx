d2349e36900f5f4228d0866078d649b2db186c52
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-15 21:48:39 +0530

    Update LeftMenu component

--- client/online-employee-management-system/src/components/LeftMenu/left-menu.component.tsx
+++ client/online-employee-management-system/src/components/LeftMenu/left-menu.component.tsx
@@ -5,68 +5,94 @@ import Button from "@/components/Button/button.component";
 import { btnList, btnList2 } from "@/constants/Basic/data";
 import Link from "next/link";
 
-// import { RootState } from "@/lib/store";
-import { useDispatch } from "react-redux";
-import { toggle } from "@/lib/features/LeftMenu/leftMenuSlice";
+import { RootState } from "@/lib/store";
+import { useSelector, useDispatch } from "react-redux";
+import { setTrue, setFalse } from "@/lib/features/LeftMenu/leftMenuSlice";
 
 export default function LeftMenu(): JSX.Element {
+  const { isOpen } = useSelector((state: RootState) => state.leftMenu);
   const dispatch = useDispatch();
   const LeftMenuRef = useRef<HTMLDivElement>(null);
 
   const collapseLeftMenu = () => {
     LeftMenuRef.current?.classList.add("-translate-x-60");
-    dispatch(toggle());
+    dispatch(setTrue());
   };
 
-  return (
-    <div
-      ref={LeftMenuRef}
-      className={`${styles.mainContainer} fixed top-20 bottom-0 w-60 shadow-2xl bg-white overflow-y-scroll transition-transform duration-700 ease-in-out`}
-    >
-      <div className={`${styles.section1} mb-8 px-3 pt-7`}>
-        {btnList.map((item, index): JSX.Element => {
-          return (
-            <Link href={item.link} key={index}>
-              <Button
-                customClassName={styles.btn}
-                customTW="text-left w-full bg-white text-black font-medium"
-              >
-                <div className="flex">
-                  <div className="my-auto mr-3">
-                    <img src={item.base64Icon} alt="" />
-                  </div>
-                  <div className={`${styles.name} text-black`}>{item.name}</div>
-                </div>
-              </Button>
-            </Link>
-          );
-        })}
-      </div>
+  const expandLeftMenu = () => {
+    LeftMenuRef.current?.classList.remove("-translate-x-60");
+    dispatch(setFalse());
+  }
 
-      <div className={`${styles.section2} px-3`}>
-        {}
-        <Button
-          customClassName={styles.btn}
-          customTW="text-left w-full bg-white text-black font-medium"
-        >
-          <div className="flex">
-            <div className="my-auto mr-3">
-              <img src={btnList2[0].base64Icon} alt="" />
+  return (
+    <div>
+      <div
+        ref={LeftMenuRef}
+        className={`${styles.leftContainer} ${
+          isOpen ? "-translate-x-60" : "translate-x-0"
+        } fixed top-20 bottom-0 w-60 shadow-2xl bg-white overflow-y-scroll transition-transform duration-300`}
+      >
+        <div className={`${styles.section1} mb-8 px-3 pt-7`}>
+          {btnList.map((item, index): JSX.Element => {
+            return (
+              <Link href={item.link} key={index}>
+                <Button
+                  customClassName={styles.btn}
+                  customTW="text-left w-full bg-white text-black font-medium"
+                >
+                  <div className="flex">
+                    <div className="my-auto mr-3">
+                      <img src={item.base64Icon} alt="" />
+                    </div>
+                    <div className={`${styles.name} text-black`}>
+                      {item.name}
+                    </div>
+                  </div>
+                </Button>
+              </Link>
+            );
+          })}
+        </div>
+
+        <div className={`${styles.section2} px-3`}>
+          {}
+          <Button
+            customClassName={styles.btn}
+            customTW="text-left w-full bg-white text-black font-medium"
+          >
+            <div className="flex">
+              <div className="my-auto mr-3">
+                <img src={btnList2[0].base64Icon} alt="" />
+              </div>
+              <div className="text-black">Logout</div>
             </div>
-            <div className="text-black">Logout</div>
-          </div>
-        </Button>
-        <Button
-          onClick={collapseLeftMenu}
-          customClassName={styles.btn}
-          customTW="text-left w-full bg-white text-black font-medium"
-        >
-          <div className="flex">
-            <div className="text-black mr-5">Collapse</div>
-            <div className="flex my-auto">
-              <img src={btnList2[1].base64Icon} alt="" />
-              <img src={btnList2[1].base64Icon} alt="" />
+          </Button>
+          <Button
+            onClick={collapseLeftMenu}
+            customClassName={styles.btn}
+            customTW="text-left w-full bg-white text-black font-medium"
+          >
+            <div className="flex">
+              <div className="text-black mr-5">Collapse</div>
+              <div className="flex my-auto">
+                <img src={btnList2[1].base64Icon} alt="" />
+                <img src={btnList2[1].base64Icon} alt="" />
+              </div>
             </div>
+          </Button>
+        </div>
+      </div>
+      <div
+        className={`${isOpen ? "block" : "hidden"} fixed top-96 left-0 z-10`}
+      >
+        <Button
+         onClick={expandLeftMenu}
+         customClassName={styles.btn}
+         customTW="text-left w-full bg-white text-black font-medium"
+        >
+          <div className="flex my-auto rotate-180">
+            <img src={btnList2[1].base64Icon} alt="" />
+            <img src={btnList2[1].base64Icon} alt="" />
           </div>
         </Button>
       </div>
