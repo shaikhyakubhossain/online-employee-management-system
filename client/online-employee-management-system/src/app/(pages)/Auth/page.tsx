71c547a60aa7a61908ce9e60326dc8fa1a52aca8
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-17 00:01:40 +0530

    Add Auth page

---
+++ client/online-employee-management-system/src/app/(pages)/Auth/page.tsx
@@ -0,0 +1,14 @@
+export default function Auth() {
+    return (
+        <div className="text-white">
+            <div className="text-3xl">
+                <div>Employeeverse</div>
+                <div className="text-2xl">Efficiently manage your workforce with our streamlined platform. Our system offers an all-in-one solution for organizing employee details, tracking performance, managing payroll, and more. Whether you’re a small business or a large enterprise, our tool ensures that you can handle all employee-related tasks with ease.</div>
+            </div>
+            <div>
+                <div>Admin Login</div>
+                <div>Employee Login</div>
+            </div>
+        </div>
+    )
+}
