4e6de5398907d2ec38c5e315e90229612c263e9b
Shaik Ahesanuddin <ahesan3954@gmail.com>
2024-09-19 21:26:00 +0530

    Add employee-model.js

---
+++ server/online-employee-management-system-backend/nodeJS/serverless/model/employee-model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    FirstName: {
+        type: String,
+        required: true,
+    },
+    LastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
