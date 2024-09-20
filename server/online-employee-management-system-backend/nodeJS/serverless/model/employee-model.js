6e18d18994803f705cfd5a4a28b98c1df8edf73f
Shaik Ahesanuddin <ahesan3954@gmail.com>
2024-09-20 16:02:36 +0530

    Update employee-model.js

--- server/online-employee-management-system-backend/nodeJS/serverless/model/employee-model.js
+++ server/online-employee-management-system-backend/nodeJS/serverless/model/employee-model.js
@@ -1,73 +1,49 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const validator = require("validator");
 
 const employeeSchema = new mongoose.Schema({
-    firstName: {
+    FirstName: {
         type: String,
         required: true,
     },
-    lastName: {
+    LastName: {
         type: String,
         required: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true,
     },
     password: {
         type: String,
         required: true,
     },
+    designation: {
+        type: String,
+        required: true,
+    },
+    employeeStatus: {
+        type: String,
+        required: true,
+    },
+    employeeType: {
+        type: String,
+        required: true,
+    },
+    State: {
+        type: String,
+        required: true,
+    },
+    genderCode: {
+        type: String,
+        required: true,
+    },
+
+    performanceScore: {
+        type: String,
+        required: true,
+    },
+    currentEmployeeRating: {
+        type: Number,
+        required: true,
+    },
 });
-
-
-employeeSchema.statics.signup = async function(firstName, lastName, email, password) {
-
-    if(!firstName || !lastName || !email){
-        return {error: 'All fields must be filled'};
-    }
-
-    if(!validator.isEmail(email)){
-        return {error: 'Email is not valid'};
-    }
-
-    const doesEmailExist = await this.findOne({email});
-    
-    if (doesEmailExist) {
-        return {error: 'Email already exists'};
-    }
-
-    if(password.length <= 5) {
-        return {error: 'Password must be at least 6 characters'};
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const employee = await this.create({email, password: hashedPassword});
-
-    return employee;
-}
-
-employeeSchema.statics.login = async function(email, password) {
-
-    const employee = await this.findOne({email});
-    
-    if (!employee) {
-        return {error: 'Employee does not exist'};
-    }
-
-    const matchPassword = await bcrypt.compare(password, employee.password);
-
-    if(!matchPassword){
-        return {error: 'Incorrect password'};
-    }
-
-    return employee
-}
-
-// const employee = mongoose.model('Employees', employeeSchema, 'employees-data');
-
-module.exports = mongoose.model('Employees', employeeSchema, 'employees-data')
