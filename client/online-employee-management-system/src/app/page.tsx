f96dca363a0d08fb76f492257743f9ac1ab18b44
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-21 20:16:56 +0530

    Update Home page

--- client/online-employee-management-system/src/app/page.tsx
+++ client/online-employee-management-system/src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
                 <div className="text-2xl max-sm:text-sm mt-5">Efficiently manage your workforce with our streamlined platform. Our system offers an all-in-one solution for organizing employee details, tracking performance, managing payroll, and more. Whether you’re a small business or a large enterprise, our tool ensures that you can handle all employee-related tasks with ease.</div>
             </div>
             <div className="flex text-2xl max-sm:text-sm">
-            <div className="mr-4"><Link href={'/Auth?type=login&role=Admin'}><Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">Admin Login</Button></Link></div>
-            <div className="ml-4"><Link href={'/Auth?type=login&role=Employee'}><Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">Employee Login</Button></Link></div>
+            <div className="mr-4"><Link href={'/Auth?type=login&role=admin'}><Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">Admin Login</Button></Link></div>
+            <div className="ml-4"><Link href={'/Auth?type=login&role=employee'}><Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">Employee Login</Button></Link></div>
             </div>
         </div>
   )
