4a58578964f33b6e3351c17f52463367356376f9
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-21 20:16:33 +0530

    Update backend

--- server/online-employee-management-system-backend/nodeJS/serverless/index.js
+++ server/online-employee-management-system-backend/nodeJS/serverless/index.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const { MongoDBUrl, PORT } = require("./secrets/api-keys");
+const Employee = require("./model/employee-model");
+const { tokenSecret } = require("./secrets/token");
+const jwt = require("jsonwebtoken");
+const requireAuth = require("./middleware/require-auth");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, tokenSecret, { expiresIn: "1d" });
+};
+
+const corsOrigin = {
+  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+};
 
 const app = express();
 
+app.use(cors(corsOrigin));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -13,19 +28,76 @@ app.use((req, res, next) => {
 
 mongoose
   .connect(MongoDBUrl)
-  .then(() => {
+  .then(async () => {
+    server = app.listen(PORT, () => {
+      console.log("Server is listening on PORT " + PORT);
+    });
+
     console.log("Connected to MongoDB");
+    // const emp = mongoose.connection.db.collection("employee");
+    // const arr = await emp.find().toArray();
+    // console.log(arr);
   })
   .catch((err) => {
     console.log(err);
   });
 
+app.post("/employee-login", async (req, res) => {
+  const { employeeId, password } = req.body;
+  console.log(employeeId, password);
+  try {
+    const employee = await Employee.login(employeeId, password);
+    if (employee.error) {
+      return res.status(400).json({ error: employee.error });
+    } else {
+      const token = createToken(employee._id);
+      const data = await Employee.findById(employee._id);
+      res.status(200).json({ data, token });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Server error" });
+  }
+});
+
+app.post("/employee-signup", async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    designation,
+    regdNo,
+    email,
+    password,
+    genderCode,
+  } = req.body;
+  try {
+    const employee = await Employee.signup(firstName, lastName, designation, regdNo, email, password, genderCode);
+    if (employee.error) {
+      return res.status(400).json({ error: employee.error });
+    } else {
+      const token = createToken(employee._id);
+      res
+        .status(200)
+        .json({
+          firstName,
+          lastName,
+          designation,
+          regdNo,
+          email,
+          genderCode,
+          token,
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Server error" });
+  }
+});
+
+app.use(requireAuth);
+
 app.get("/", (req, res) => {
   res.send({
     message: "hello",
   });
-});
-
-const server = app.listen(PORT, () => {
-  console.log("Server is listening on PORT " + PORT);
 });
--- server/online-employee-management-system-backend/nodeJS/serverless/model/employee-model.js
+++ server/online-employee-management-system-backend/nodeJS/serverless/model/employee-model.js
@@ -1,102 +1,120 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const autoSequence = require("mongoose-sequence")(mongoose);
 
 const employeeSchema = new mongoose.Schema({
-    employeeId: {
-        type: Number,
-        unique: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    LastName: {
-        type: String,
-        required: true,
-    },
-    designation: {
-        type: String,
-        required: true,
-    },
-    regdNo: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    genderCode: {
-        type: String,
-        required: true,
-    },
+  employeeId: {
+    type: Number,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  LastName: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  regdNo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  genderCode: {
+    type: String,
+    required: true,
+  },
 });
 
+employeeSchema.statics.signup = async function (
+  firstName,
+  lastName,
+  designation,
+  regdNo,
+  email,
+  password,
+  genderCode
+) {
+  if (
+    !firstName ||
+    !lastName ||
+    !designation ||
+    !regdNo ||
+    !email ||
+    !genderCode
+  ) {
+    return { error: "All fields must be filled" };
+  }
 
-employeeSchema.statics.signup = async function(firstName, lastName, designation, regdNo, email, password, genderCode) {
+  if (!validator.isEmail(email)) {
+    return { error: "Email is not valid" };
+  }
 
-    if(!firstName || !lastName || !designation || !regdNo || !email || !genderCode) {
-        return {error: 'All fields must be filled'};
-    }
+  const doesEmailExist = await this.findOne({ email });
+  const doesRegdNoExist = await this.findOne({ regdNo });
 
-    if(!validator.isEmail(email)){
-        return {error: 'Email is not valid'};
-    }
+  if (doesEmailExist) {
+    return { error: "Email already exists" };
+  }
 
-    const doesEmailExist = await this.findOne({email});
-    const doesRegdNoExist = await this.findOne({regdNo});
-    
-    if (doesEmailExist) {
-        return {error: 'Email already exists'};
-    }
+  if (doesRegdNoExist) {
+    return { error: "Regd No already exists" };
+  }
 
-    if (doesRegdNoExist) {
-        return {error: 'Regd No already exists'};
-    }
+  if (password.length <= 5) {
+    return { error: "Password must be at least 6 characters" };
+  }
 
-    if(password.length <= 5) {
-        return {error: 'Password must be at least 6 characters'};
-    }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  const employee = await this.create({
+    firstName,
+    lastName,
+    designation,
+    regdNo,
+    genderCode,
+    email,
+    password: hashedPassword,
+  });
 
-    const employee = await this.create({
-        firstName,
-        lastName,
-        designation,
-        regdNo,
-        genderCode,
-        email,
-        password: hashedPassword
-    });
+  return employee;
+};
 
-    return employee;
-}
+employeeSchema.statics.login = async function (employeeId, password) {
+  const employee = await this.findOne({ employeeId });
 
-employeeSchema.statics.login = async function(email, password) {
+  if (!employee) {
+    return { error: "Employee does not exist" };
+  }
 
-    const employee = await this.findOne({email});
-    
-    if (!employee) {
-        return {error: 'Employee does not exist'};
-    }
-
+  if (employee.password.slice(0, 4) === "$2b$") {
     const matchPassword = await bcrypt.compare(password, employee.password);
 
-    if(!matchPassword){
-        return {error: 'Incorrect password'};
+    if (!matchPassword) {
+      return { error: "Incorrect password" };
     }
+  }
+  else if(employee.password !== password){
+    return { error: "Incorrect password" };
+  }
 
-    return employee
-}
+  return employee;
+};
 
 // const employee = mongoose.model('Employees', employeeSchema, 'employees-data');
 
-module.exports = mongoose.model('Employees', employeeSchema, 'employee')
+module.exports = mongoose.model("Employees", employeeSchema, "employee");
