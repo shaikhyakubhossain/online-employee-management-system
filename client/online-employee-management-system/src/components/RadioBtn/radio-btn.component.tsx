0dfdbe7fedfc4944bd1f3922abb02f1a41985dd6
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-27 16:33:35 +0530

    Add RadioBtn component, update button links in Basic data

---
+++ client/online-employee-management-system/src/components/RadioBtn/radio-btn.component.tsx
@@ -0,0 +1,20 @@
+type propsType = {
+  label: string;
+  style?: string;
+}
+
+export default function RadioBtn(props: propsType): JSX.Element {
+  return (
+    <div className={`flex items-center ps-4 border px-5 bg-gray-50 border-gray-200 rounded dark:border-gray-700`}>
+      <input
+        type="radio"
+        value=""
+        name="bordered-radio"
+        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      />
+      <label className="w-full py-4 ms-2 text-sm font-medium dark:text-gray-900 text-gray-300">
+        {props.label}
+      </label>
+    </div>
+  );
+}
