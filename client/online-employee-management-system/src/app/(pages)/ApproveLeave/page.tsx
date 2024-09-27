6c84da7276e54f71a3de21b1d127cef1ec4626d1
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-27 21:56:27 +0530

    Add ApproveLeave page, add SearchBox component

---
+++ client/online-employee-management-system/src/app/(pages)/ApproveLeave/page.tsx
@@ -0,0 +1,10 @@
+import SearchBox from "@/components/ApproveLeave/SearchBox/search-box.component";
+
+export default function ApproveLeave() {
+    return (
+        <div className="">
+            <div className="text-5xl mb-12 font-semibold border-b-2 border-gray-800 text-blue-900">Approve Leave </div>
+            <SearchBox />
+        </div>
+    )
+}
