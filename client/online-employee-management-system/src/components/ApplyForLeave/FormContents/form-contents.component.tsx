45c14ff624ad9eba8f97455ab52e0b86e6fc5830
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-27 21:05:28 +0530

    Update FormContents component and SetDate component

--- client/online-employee-management-system/src/components/ApplyForLeave/FormContents/form-contents.component.tsx
+++ client/online-employee-management-system/src/components/ApplyForLeave/FormContents/form-contents.component.tsx
@@ -1,32 +1,59 @@
 "use client";
+import { useState, useEffect } from "react";
 import styles from "./form-contents.module.scss";
 import RadioBtn from "@/components/RadioBtn/radio-btn.component";
 import SetDate from "../SetDate/set-date.component";
 import Link from "next/link";
 import Button from "@/components/Button/button.component";
 
+type dataToSendType = {
+    leaveType: string;
+    fromDate: string | undefined;
+    toDate: string | undefined;
+    additionalInfo?: string;
+}
+
+type propsType = {
+
+}
+
 export default function FormContents() {
+
+    const [dataToSend, setDataToSend] = useState<dataToSendType>({
+        leaveType: "",
+        fromDate: "",
+        toDate: "",
+        additionalInfo: "",
+    });
+
+    const handleSubmit = () => {
+
+    }
+
+    console.log("dataToSend", dataToSend);
+
   return (
     <div className="">
       <div className="text-2xl font-semibold">Choose type of leave :</div>
       <div className="flex justify-evenly flex-wrap my-5">
-        <RadioBtn label="Casual Leave" />
-        <RadioBtn label="Pay Leave" />
-        <RadioBtn label="Sick Leave" />
-        <RadioBtn label="Maternity Leave" />
+        <RadioBtn label="Casual Leave" onRadioClick={() => setDataToSend({ ...dataToSend, leaveType: "casual" })} />
+        <RadioBtn label="Pay Leave" onRadioClick={() => setDataToSend({ ...dataToSend, leaveType: "pay" })} />
+        <RadioBtn label="Sick Leave" onRadioClick={() => setDataToSend({ ...dataToSend, leaveType: "sick" })} />
+        <RadioBtn label="Maternity Leave" onRadioClick={() => setDataToSend({ ...dataToSend, leaveType: "maternity" })} />
       </div>
       <div className="text-2xl font-semibold">Set range</div>
       <div className="flex my-5">
-        <SetDate label="From" />
-        <SetDate label="To" />
+        <SetDate updateDataToSend={(event) => setDataToSend({ ...dataToSend, fromDate: event.target.value})} label="From" />
+        <SetDate updateDataToSend={(event) => setDataToSend({ ...dataToSend, toDate: event.target.value})} label="To" />
       </div>
-      <div className="my-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900">
+      <div className="mt-12 mb-3">
+        <label className="block mb-2 text-2xl font-semibold">
           Additional info : (Optional)
         </label>
         <textarea
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Write additional info here..."
+          onChange={(event) => setDataToSend({ ...dataToSend, additionalInfo: event.target.value })}
         ></textarea>
       </div>
       <div>
