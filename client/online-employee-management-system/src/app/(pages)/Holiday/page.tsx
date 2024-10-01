50624c8fe4b9d6b6850528ef02384b5e35d33228
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-01 05:44:26 +0530

    Update pages

--- client/online-employee-management-system/src/app/(pages)/Holiday/page.tsx
+++ client/online-employee-management-system/src/app/(pages)/Holiday/page.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-
-const HolidayPage: React.FC = () => {
+export default function Holiday() {
     return (
         <div>
             <h1>Holiday Page</h1>
             <p>Welcome to the Holiday Page. Here you can find information about holidays.</p>
-            {/* Add more content and components as needed */}
         </div>
     );
 };
-
-export default HolidayPage;
