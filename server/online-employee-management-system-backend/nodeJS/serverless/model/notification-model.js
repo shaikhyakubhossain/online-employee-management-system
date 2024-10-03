0fec2b4375b5eb1ea30315a067da35b4616cc46f
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-03 22:02:37 +0530

    Add notification-model.js

---
+++ server/online-employee-management-system-backend/nodeJS/serverless/model/notification-model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const autoSequence = require("mongoose-sequence")(mongoose);
+
+const notificationSchema = new mongoose.Schema({
+  notificationId: {
+    type: Number,
+    unique: true,
+  },
+  regdNo: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+notificationSchema.plugin(autoSequence, { inc_field: "notificationId" });
+
+notificationSchema.statics.createNotification = async function (
+  regdNo,
+  title,
+  message
+) {
+  if (!regdNo || !message || !title) {
+    return { error: "user not logged in" };
+  }
+
+  const notification = await this.create({
+    regdNo,
+    title,
+    message,
+  });
+
+  return notification;
+};
+
+module.exports = mongoose.model("Notification", notificationSchema, "notification");
