cecb38774198d9c9e407ca27a5301249cc20bfb6
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-05 03:11:58 +0530

    Add Section component

---
+++ client/online-employee-management-system/src/components/Notice/Section/section.component.tsx
@@ -0,0 +1,23 @@
+import styles from "./section.module.scss";
+
+type propsType = {
+    title: string;
+    // data: any;
+}
+
+export default function Section(props: propsType): JSX.Element {
+    return (
+        <div className={`${styles.mainContainer} my-6`}>
+            <div className={`${styles.sectionTitle} text-red-500 text-2xl font-semibold`}>{props.title}</div>
+            <div className={`${styles.sectionBody}`}>
+                {
+                    [...Array(2)].map((_, index) => (
+                        <div className={`${styles.notice} text-blue-700 font-semibold my-4`} key={index}>
+                            • Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
