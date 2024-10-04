496a4aff25cddbb699d0102b61229b4a4133b9a8
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-05 03:09:19 +0530

    Update Notification page

--- client/online-employee-management-system/src/app/(pages)/Notification/page.tsx
+++ client/online-employee-management-system/src/app/(pages)/Notification/page.tsx
@@ -3,6 +3,7 @@ import MainBody from "@/components/Notification/MainBody/main-body.component";
 export default function Notification(){
     return(
         <div>
+            <div className="text-5xl mb-12 font-semibold border-b-2 border-gray-800 text-blue-900">Notification</div>
             <MainBody/>
         </div>
     )
