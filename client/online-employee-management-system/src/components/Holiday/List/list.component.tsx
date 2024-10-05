b80178b5c8788ab2a87aab019454425029ddaeeb
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-06 01:45:32 +0530

    Add List component

---
+++ client/online-employee-management-system/src/components/Holiday/List/list.component.tsx
@@ -0,0 +1,21 @@
+import type { Holiday } from "@/constants/Types/response-data";
+
+type propsType = {
+  holidays: Holiday[];
+};
+
+export default function List(props: propsType) {
+  const { holidays } = props;
+  return (
+    <div className="mb-8">
+      {holidays.map(
+        (holiday, index): JSX.Element => (
+          <div key={index} className="py-1">
+            <strong>{new Date(holiday.date).toDateString()}:</strong>{" "}
+            {holiday.localName}
+          </div>
+        )
+      )}
+    </div>
+  );
+}
