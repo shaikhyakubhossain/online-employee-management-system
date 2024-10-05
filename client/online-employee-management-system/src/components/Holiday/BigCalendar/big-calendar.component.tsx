684cf839297809e2a1d7f5cfbb886b587ee1d28c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-06 01:45:03 +0530

    Add BigCalendar component

---
+++ client/online-employee-management-system/src/components/Holiday/BigCalendar/big-calendar.component.tsx
@@ -0,0 +1,39 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import type { Holiday } from "@/constants/Types/response-data";
+
+type propsType = {
+    handleNavigate: (newDate: Date) => void;
+    handleEventClick: (event: { title: string }) => void;
+    date: Date;
+    calenderEvents: Holiday[];
+}
+
+const localizer = momentLocalizer(moment);
+
+export default function BigCalendar(props: propsType) {
+    
+      const events = props.calenderEvents.map((holiday) => ({
+        title: holiday.name,
+        start: new Date(holiday.date),
+        end: new Date(holiday.date),
+      }));
+
+    return (
+        <div className="w-full h-96 bg-white rounded-lg shadow-lg p-4">
+            <Calendar
+              localizer={localizer}
+              events={events}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: "100%", width: "100%" }}
+              className="rounded-lg"
+              views={["month"]}
+              date={props.date}
+              onNavigate={props.handleNavigate}
+              onSelectEvent={props.handleEventClick}
+            />
+          </div>
+    )
+}
