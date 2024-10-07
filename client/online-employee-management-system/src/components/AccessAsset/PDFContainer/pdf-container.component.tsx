d4532996d59e1d94b01063f268217a6274c3cf1e
Samarjit Sahoo <samarjit9203@gmail.com>
2024-10-07 21:10:49 +0530

    Apply Responsiveness for some Pages

--- client/online-employee-management-system/src/components/AccessAsset/PDFContainer/pdf-container.component.tsx
+++ client/online-employee-management-system/src/components/AccessAsset/PDFContainer/pdf-container.component.tsx
@@ -2,10 +2,10 @@ export default function PDFContainer(): JSX.Element {
   return (
     <div>
       <iframe
-        className="rounded-lg mx-auto my-5"
+        className="rounded-lg mx-auto my-5 "
         src="https://drive.google.com/file/d/1ZP6pRH-EyvyEhyeoKVowRXDVoGhB5v90/preview"
-        width="480"
-        height="640"
+        width="250"
+        height="250"
         allow="autoplay"
       ></iframe>
     </div>
