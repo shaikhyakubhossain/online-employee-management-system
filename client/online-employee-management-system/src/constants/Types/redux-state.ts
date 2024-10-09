31477142cb19744d8fc98bbdae620d27b922b26d
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-09 11:46:10 +0530

    Update redux-state.ts

--- client/online-employee-management-system/src/constants/Types/redux-state.ts
+++ client/online-employee-management-system/src/constants/Types/redux-state.ts
@@ -2,9 +2,11 @@ export type authDetailDataType = {
     employeeId: string;
     firstName: string;
     lastName: string;
+    username: string;
     designation: string;
     email: string;
     password: string;
     regdNo: string;
+    genderCode: string;
     token: string;
 };
