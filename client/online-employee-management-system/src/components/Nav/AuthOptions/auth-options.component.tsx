2612a105003113381ed44a6e4d7d761194974f3b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-11 00:36:47 +0530

    Add ContactUs page, update AuthOptions, and MainBody component, refactor code

--- client/online-employee-management-system/src/components/Nav/AuthOptions/auth-options.component.tsx
+++ client/online-employee-management-system/src/components/Nav/AuthOptions/auth-options.component.tsx
@@ -5,9 +5,10 @@ export default function AuthOptions() {
     return (
         <div className={`${styles.mainContainer} flex text-2xl text-white`}>
             <div><Link href={'/Auth?type=login&role=employee'}><div>Login</div></Link></div>
-            <div><Link href={'#'}><div>About us</div></Link></div>
+            <div><Link href={'/AboutUs'}><div>About us</div></Link></div>
             <div><Link href={'/Auth?type=signup&role=Admin'}><div>Register</div></Link></div>
-            <div><Link href={'#'}><div>Help</div></Link></div>
+            <div><Link href={'/Help'}><div>Help</div></Link></div>
+            <div><Link href={'/ContactUs'}><div>Contact us</div></Link></div>
         </div>
     )
 }
