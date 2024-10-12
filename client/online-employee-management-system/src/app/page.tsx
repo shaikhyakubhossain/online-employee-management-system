81c16a7add564b51ee73a7f2bf90b01f6d4d8a65
Samarjit Sahoo <samarjit9203@gmail.com>
2024-10-12 07:29:32 +0530

    🔥Added ChatBot🔥

--- client/online-employee-management-system/src/app/page.tsx
+++ client/online-employee-management-system/src/app/page.tsx
@@ -2,36 +2,48 @@ import Link from "next/link";
 import Button from "@/components/Button/button.component";
 import HeroHeaderHome from "@/components/HeroHeaderHome/hero-header-home.component";
 import { MainBackgroundImg } from "@/components/Auth/MainBackgroundImg/main-background-img.component";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <div
-      className="relative text-white p-16 overflow-y-scroll"
-      style={{
-        background: "rgba(20, 19, 112, 0.85)",
-        height: "calc(100dvh - 80px)",
-        fontFamily: "'Times New Roman', serif",
-        scrollbarWidth: "none",
-      }}
-    >
-      <MainBackgroundImg />
-      <HeroHeaderHome />
-      <div className="flex flex-wrap text-2xl max-sm:text-sm mt-6">
-        <div className="mx-4 my-2">
-          <Link href={"/Auth?type=login&role=admin"}>
-            <Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">
-              Admin Login
-            </Button>
-          </Link>
-        </div>
-        <div className="mx-4 my-2">
-          <Link href={"/Auth?type=login&role=employee"}>
-            <Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">
-              Employee Login
-            </Button>
-          </Link>
+    <>
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://files.bpcontent.cloud/2024/10/11/09/20241011093608-RK2S5C9N.js"
+        strategy="afterInteractive"
+      />
+
+      <div
+        className="relative text-white p-16 overflow-y-scroll"
+        style={{
+          background: "rgba(20, 19, 112, 0.85)",
+          height: "calc(100dvh - 80px)",
+          fontFamily: "'Times New Roman', serif",
+          scrollbarWidth: "none",
+        }}
+      >
+        <MainBackgroundImg />
+        <HeroHeaderHome />
+        <div className="flex flex-wrap text-2xl max-sm:text-sm mt-6">
+          <div className="mx-4 my-2">
+            <Link href={"/Auth?type=login&role=admin"}>
+              <Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">
+                Admin Login
+              </Button>
+            </Link>
+          </div>
+          <div className="mx-4 my-2">
+            <Link href={"/Auth?type=login&role=employee"}>
+              <Button customTW="bg-blue-500 rounded-xl hover:bg-blue-400 hover:text-white px-8 py-4">
+                Employee Login
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
