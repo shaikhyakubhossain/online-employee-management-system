ea9b5415dee6a0bf6ac15afdef86790c9c57aaea
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-13 09:26:04 +0530

    Update MainBody component

--- client/online-employee-management-system/src/components/Notice/MainBody/main-body.component.tsx
+++ client/online-employee-management-system/src/components/Notice/MainBody/main-body.component.tsx
@@ -1,10 +1,20 @@
 "use client";
 import Section from "../Section/section.component";
 
+import { RootState } from "@/lib/store";
+import { useSelector } from "react-redux";
+
 export default function MainBody() {
+
+    const { role } = useSelector((state: RootState) => state.authDetail);
 
     return (
         <div>
+            {
+                role === "admin" && (
+                    <div className="text-lg my-6"><span className="text-red-500 bg-slate-200 p-3 rounded-lg cursor-pointer hover:bg-slate-300">Add Notice</span></div>
+                )
+            }
             <Section title="Latest Notices" />
             <Section title="Other Notices" />
         </div>
--- client/online-employee-management-system/src/components/Resign/MainBody/main-body.component.tsx
+++ client/online-employee-management-system/src/components/Resign/MainBody/main-body.component.tsx
@@ -2,7 +2,7 @@ export default function Resign() {
     return (
         <div className="font-times">
            <div className="text-3xl text-red-500 bg-slate-200 p-3 rounded-lg">Danger Zone</div>
-           <div className="text-lg text-red-600 my-6"><span className="bg-black p-3 rounded-lg cursor-pointer">Resign</span></div>
+           <div className="text-lg text-red-600 my-6"><span className="bg-black hover:bg-white p-3 rounded-lg cursor-pointer border-2 border-red-500">Resign</span></div>
         </div>
     );
 }
