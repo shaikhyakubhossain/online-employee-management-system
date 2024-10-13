ff7d110ec94cc9b981a20e41348a5fb98826adfc
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-13 08:58:29 +0530

    Add MainBody component, update Resign page

--- client/online-employee-management-system/src/app/(pages)/Resign/page.tsx
+++ client/online-employee-management-system/src/app/(pages)/Resign/page.tsx
@@ -1,11 +1,11 @@
 import PageHeader from "@/components/PageHeader/page-header.component";
+import MainBody from "@/components/Resign/MainBody/main-body.component";
 
 export default function Resign() {
     return (
         <div className="font-times">
             <PageHeader title="Resign" />
-            <div className="text-3xl text-red-500 bg-slate-200 p-3 rounded-lg">Danger Zone</div>
-            <div className="text-lg text-red-600"><span>Give Resign</span></div>
+            <MainBody />
         </div>
     );
 }
