cd6c2a53a6c9cc578b3d9c3937dc64c334c41c7c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-19 15:26:59 +0530

    Update AddNotice component

--- client/online-employee-management-system/src/components/Notice/AddNotice/add-notice.component copy.tsx
+++ client/online-employee-management-system/src/components/Notice/AddNotice/add-notice.component copy.tsx
@@ -8,16 +8,18 @@ export default function AddNotice(props: propsType) {
   return (
     <div className="">
       <div>
-        <div className="text-2xl">Add Notice</div>
+        <div className="text-2xl font-semibold text-red-500">Add Notice</div>
+        <label className="text-lg font-semibold">Title:</label><br />
         <input
-          className="border-2 border-black m-2 rounded-md bg-red-100"
+          className="border-2 p-2 shadow-xl shadow-slate-200 m-2 rounded-md bg-red-50"
           onChange={(event) => props.updateTitle(event.target.value)}
           type="text"
           placeholder="Title"
         />
         <br />
+        <label className="text-lg font-semibold">Body:</label><br />
         <textarea
-          className="border-2 border-black rounded-md bg-red-100 m-2 w-full h-28"
+          className="border-2 p-2 shadow-xl shadow-slate-200 rounded-md bg-red-50 m-2 w-full h-28"
           onChange={(event) => props.updateMessage(event.target.value)}
           placeholder="Body"
         />
@@ -25,7 +27,7 @@ export default function AddNotice(props: propsType) {
       <div className="text-lg my-6">
         <span
           onClick={props.handleAddNotice}
-          className="text-red-500 bg-slate-200 p-3 rounded-lg cursor-pointer hover:bg-slate-300"
+          className="text-red-500 hover:text-red-200 bg-slate-200 p-3 rounded-lg cursor-pointer hover:bg-slate-600"
         >
           submit
         </span>
