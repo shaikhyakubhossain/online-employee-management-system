3d983185b67ce0740defc403de927acf63c2ee8c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-21 06:57:46 +0530

    Update AddFile component

--- client/online-employee-management-system/src/components/AccessAsset/AddFile/add-file.component.tsx
+++ client/online-employee-management-system/src/components/AccessAsset/AddFile/add-file.component.tsx
@@ -4,21 +4,23 @@ import Button from "@/components/Button/button.component";
 import styles from "./add-file.module.scss";
 
 export default function AddFile() {
+  const [showDropDown, setShowDropDown] = useState(false);
 
-    const [showDropDown, setShowDropDown] = useState(false);
-
-    return (
-        <div className="relative flex">
-            <div className="">
-            <div className=" text-2xl font-semibold">Add File:</div>
-            <input type="file" />
-            </div>
-            <Button onClick={() => setShowDropDown(!showDropDown)}>File Type</Button>
-            <div className={`${styles.dropDown} absolute top-11 left-[250px] bg-gray-700 p-2 text-white rounded-md`} style={{ display: showDropDown ? "block" : "none" }}>
-                <div className="text-xl font-semibold border-b-2 border-gray-400">Select Type: </div>
-                <div>Study Materials (Notes)</div>
-                <div>Interview Questions</div>
-            </div>
+  return (
+    <div className={`${styles.mainContainer} relative flex`}>
+      <div className=" text-2xl font-semibold m-2">Add File:</div>
+      <Button onClick={() => setShowDropDown(!showDropDown)} customTW="m-2">File Type</Button>
+      <div
+        className={`${styles.dropDown} absolute top-14 left-[120px] bg-white border-gray-400 border p-2 text-black rounded-md`}
+        style={{ display: showDropDown ? "block" : "none" }}
+      >
+        <div className="text-xl font-semibold border-b-2 border-gray-400">
+          Select Type:{" "}
         </div>
-    );
+        <div>Study Materials (Notes)</div>
+        <div>Interview Questions</div>
+      </div>
+      <input className="m-2 self-center" type="file" />
+    </div>
+  );
 }
