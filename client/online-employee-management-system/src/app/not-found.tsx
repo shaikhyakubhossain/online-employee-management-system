c222f22122228cfaf985fda237c78a967a03d911
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-11-05 11:51:46 +0530

    Add NotFound page

---
+++ client/online-employee-management-system/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="h-[calc(100dvh-160px)] flex flex-col items-center justify-center">
+            <div>404 - Page Not Found</div>
+        </div>
+    );
+}
