730a1fc51850c771e26e582296d3014d2b863973
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-03-22 18:29:17 +0530

    Update Nav component: replace text with logo and change background color

--- client/online-employee-management-system/src/components/Nav/nav.component.tsx
+++ client/online-employee-management-system/src/components/Nav/nav.component.tsx
@@ -1,12 +1,6 @@
 import styles from "./nav.module.scss";
 import Link from "next/link";
 import RightContainer from "./RightContainer/right-container.component";
-import { Pacifico } from "next/font/google";
-
-const pacifico = Pacifico({
-  weight: "400",
-  subsets: ["latin"],
-});
 
 export default function Nav() {
   return (
@@ -14,7 +8,13 @@ export default function Nav() {
       className={`${styles.mainContainer} fixed top-0 left-0 right-0 flex justify-between items-center h-20 px-6 text-white text-2xl`}
     >
       <Link href={"/"}>
-        <div className={pacifico.className}>Employeeverse</div>
+        <div>
+          <img
+            className="w-[182px] h-[83px] object-cover"
+            src="https://s3-alpha-sig.figma.com/img/d644/c1c7/54af1fc9068714e8a816c1c73476a052?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=XE1CwOrRG1TvfWjiSNgGc6XWLUwbK9csJQQHx6W498M7wIalJnAcnyaGmMIYe93O~BKLHiDoTwIfh8pPSOX5f36R4BSXSajCOn3YKjJuXk9-bAFRz9X3bChzuPhZku3zV~OAB5JVfyehI6MkPhUwb2e3BtXk8UBO5nCmbl-V5RvHb9I2DtqxfHcY8ilwJ2VdUHpgshPyVQTf9mrGXrgSLge5E3tWIuNBFp4UG9vFhGx4Njgoqs5YYgybOoCEeZwm2JX0saMUZX62YCXGEraLwNizszsYvidY65ItCK6v1IZChe5bvX-jFeu3ypmOUlomzE8vwB2uUnQmfOM8ymb34Q__"
+            alt=""
+          />
+        </div>
       </Link>
       <RightContainer />
     </div>
