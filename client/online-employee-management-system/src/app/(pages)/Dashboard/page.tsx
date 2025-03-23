2c81ae4638cb136ce705e0ef691c61e472397185
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-03-23 17:45:41 +0530

    Refactor Dashboard component by commenting out unused code and removing state management for top employee

--- client/online-employee-management-system/src/app/(pages)/Dashboard/page.tsx
+++ client/online-employee-management-system/src/app/(pages)/Dashboard/page.tsx
@@ -1,31 +1,31 @@
 "use client"; // Add this at the top
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import HeroHeader from "@/components/Dashboard/HeroHeader/hero-header.component";
 import HeroContent from "@/components/Dashboard/HeroContent/hero-content.component";
 import SubHeaderContainer from "@/components/Dashboard/SubHeaderContainer/sub-header-container.component";
 
-interface Employee {
-  employeeId: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-}
+// interface Employee {
+//   employeeId: string;
+//   username: string;
+//   firstName: string;
+//   lastName: string;
+// }
 
 export default function Dashboard() {
-  const [topEmployee, setTopEmployee] = useState<Employee | null>(null);
+  // const [topEmployee, setTopEmployee] = useState<Employee | null>(null);
 
   useEffect(() => {
-    fetch(
-      "https://online-employee-management-system.onrender.com/top-employees"
-    ) // Fetch data from FastAPI backend
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.length > 0) {
-          setTopEmployee(data[0]); // Get the highest-ranked employee
-        }
-      })
-      .catch((err) => console.error("Error fetching top employee:", err));
+    // fetch(
+    //   "https://online-employee-management-system.onrender.com/top-employees"
+    // ) // Fetch data from FastAPI backend
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.length > 0) {
+    //       setTopEmployee(data[0]); // Get the highest-ranked employee
+    //     }
+    //   })
+    //   .catch((err) => console.error("Error fetching top employee:", err));
   }, []);
 
   return (
@@ -38,7 +38,7 @@ export default function Dashboard() {
         showCurrentUserName={true}
       />
       <SubHeaderContainer />
-      <HeroHeader
+      {/* <HeroHeader
         greeting="We congratulate"
         title="Employee of the Year"
         message={
@@ -49,7 +49,7 @@ export default function Dashboard() {
         showEmoji={false}
         showCurrentUserName={false}
         swapTextLocation={true}
-      />
+      /> */}
       <HeroContent />
     </div>
   );
