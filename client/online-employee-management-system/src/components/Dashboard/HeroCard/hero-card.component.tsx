101068f5bd418500577fe4abfb863a2cf717ba37
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-04-04 09:06:20 +0530

    Refactor HeroCard component for improved layout and readability; adjust structure for better alignment of elements

--- client/online-employee-management-system/src/components/Dashboard/HeroCard/hero-card.component.tsx
+++ client/online-employee-management-system/src/components/Dashboard/HeroCard/hero-card.component.tsx
@@ -7,24 +7,32 @@ type propsType = {
 };
 
 export default function HeroCard(props: propsType): JSX.Element {
-
   return (
     <Link href={props.btnList.link}>
-    <div className={`${styles.mainContainer} p-8 rounded-lg cursor-pointer`} style={{ backgroundColor: props.btnList.bg + "25"}}>
-      <div className={`${styles.row1} mb-10`}>
-        <div className="flex justify-between">
-          <div className={`${styles.name} font-bold text-xl`} style={{ color: props.btnList.bg }}>{props.btnList.name}</div>
+      <div
+        className={`${styles.mainContainer} p-8 rounded-lg cursor-pointer`}
+        style={{ backgroundColor: props.btnList.bg + "25" }}
+      >
+        <div className={`${styles.row1} mb-10`}>
+          <div className="flex justify-between">
+            <div
+              className={`${styles.name} font-bold text-xl`}
+              style={{ color: props.btnList.bg }}
+            >
+              {props.btnList.name}
+            </div>
+            <div>
+              <img className="w-5" src={props.btnList.base64Icon} alt="" />
+            </div>
+          </div>
+          <div>{props.btnList.detail}</div>
+        </div>
+        <div className={`${styles.row2}`}>
+          <div className="text-xs text-slate-500">People Added</div>
           <div>
-            <img className="w-5" src={props.btnList.base64Icon} alt="" />
           </div>
         </div>
-        <div>{props.btnList.detail}</div>
       </div>
-      <div className={`${styles.row2}`}>
-        <div className="text-xs text-slate-500">People Added</div>
-        <div></div>
-      </div>
-    </div>
     </Link>
   );
 }
