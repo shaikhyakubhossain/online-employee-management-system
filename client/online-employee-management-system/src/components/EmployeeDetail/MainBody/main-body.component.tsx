1b4e4bb10e0e7335d77ffbee64784a3bb43e6ca2
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-04-07 19:57:11 +0530

    Implement pagination and search functionality in MainBody and EmployeeDetail components; add PaginationBar component and update data fetching logic to support pagination and specific search queries

--- client/online-employee-management-system/src/components/EmployeeDetail/MainBody/main-body.component.tsx
+++ client/online-employee-management-system/src/components/EmployeeDetail/MainBody/main-body.component.tsx
@@ -1,41 +1,40 @@
 "use client";
 // import styles from "./main-body.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchBox from "@/components/SearchBox/search-box.component";
-import Button from "@/components/Button/button.component";
 import Table from "@/components/Table/table.component";
 import type { employeeData } from "@/constants/Types/response-data";
-import { btnList2 } from "@/constants/Basic/data";
 import useFetchGetMethod from "@/hooks/FetchMethods/useFetchGetMethod";
 import DetailModal from "@/components/DetailModal/detail-modal.component";
+import PaginationBar from "@/components/PaginationBar/pagination-bar.component";
+
+type serverData = {
+  data: employeeData[];
+  pageCount: number | null;
+}
 
 export default function MainBody() {
-  const [data, setData] = useState<employeeData[] | null>(null);
+  const [data, setData] = useState<serverData | null>(null);
   const [modalDetailToShow, setModalDetailToShow] =
     useState<employeeData | null>(null);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [searchResults, setSearchResults] = useState<employeeData[]>([]);
   const [searchData, setSearchData] = useState<string>("");
-  const [page, setPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(0);
 
   useFetchGetMethod(
     `get-all-employees`,
     "admin",
-    (data: employeeData[] | null) => setData(data),
+    (data: serverData | null) => setData(data),
     false,
-    page
+    page,
+    searchData
   );
 
-  console.log(data);
+  useEffect(() => {
+    setPage(0);
+  }, [searchData]);
 
-  const handleSearch = () => {
-    if (!data) return;
-    const localData: employeeData[] = data.filter((item) =>
-      item.firstName.includes(searchData)
-    );
-    // console.log("searching: ", searchData);
-    setSearchResults(localData);
-  };
+  console.log(data);
 
   return (
     <div>
@@ -45,48 +44,23 @@ export default function MainBody() {
           hide={() => setShowModal(false)}
         />
       )}
-      <SearchBox
-        updateSearchData={(data: string) => setSearchData(data)}
-        startSearch={handleSearch}
-      />
+      <SearchBox updateSearchData={(data: string) => setSearchData(data)} />
       <Table
         handleRowDetailToShowInModal={(row) => {
           setModalDetailToShow(row);
           setShowModal(true);
         }}
-        data={searchResults.length > 0 ? searchResults : data}
+        data={data && data.data}
         headers={["Employee Name", "Regd.ID", "Email ID", "Designation"]}
         showAction={false}
       />
-      <div className="flex justify-center items-center gap-3">
-        <Button
-          disabled={page === 1}
-          onClick={() => setPage((prev) => prev - 1)}
-        >
-          <img src={btnList2[1].base64Icon} alt="" />
-        </Button>
-        <div>
-          <input
-            className="clean-number-input w-12 text-center"
-            type="number"
-            defaultValue={page}
-            value={page}
-            onChange={(e) => {
-              const value = parseInt(e.target.value);
-              if (value >= 1 && value <= 1000) {
-                setPage(parseInt(e.target.value));
-              }
-            }}
-          />
-        </div>
-        <Button
-          disabled={page === 1000}
-          onClick={() => setPage((prev) => prev + 1)}
-        >
-          <img className="rotate-180" src={btnList2[1].base64Icon} alt="" />
-        </Button>
-      </div>
-      {/* <div className="flex justify-center items-center pt-5"><Button onClick={() => setPage(prev => prev + 1)}>Load More</Button></div> */}
+      <PaginationBar
+        page={page}
+        pageCount={data && data.pageCount}
+        incrementPage={(value) => setPage(value)}
+        decrementPage={(value) => setPage(value)}
+        setCustomPage={(value) => setPage(value)}
+      />
     </div>
   );
 }
--- client/online-employee-management-system/src/components/SearchBox/search-box.component.tsx
+++ client/online-employee-management-system/src/components/SearchBox/search-box.component.tsx
@@ -28,7 +28,7 @@ export default function SearchBox(props: propsType) {
           </svg>
         </div>
         <input
-        onChange={(event) => props.updateSearchData && props.updateSearchData(event.target.value)}
+        // onChange={(event) => props.updateSearchData && props.updateSearchData(event.target.value)}
           type="search"
           id="default-search"
           className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-600 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
@@ -36,7 +36,7 @@ export default function SearchBox(props: propsType) {
           required
         />
         <button
-        onClick={props.startSearch}
+        onClick={(event) => props.updateSearchData && props.updateSearchData(((event.currentTarget as HTMLButtonElement)?.previousSibling as HTMLInputElement).value)}
           type="submit"
           className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
