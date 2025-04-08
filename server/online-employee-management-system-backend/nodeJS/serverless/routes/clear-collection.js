be395bb03cd9c573d88549c44b1b98b87f0ce150
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-04-08 17:53:34 +0530

    Remove authorization check from clearCollection function

--- server/online-employee-management-system-backend/nodeJS/serverless/routes/clear-collection.js
+++ server/online-employee-management-system-backend/nodeJS/serverless/routes/clear-collection.js
@@ -1,16 +1,10 @@
 const { setModel } = require("../utils/methods");
 
 const clearCollection = async (req, res, collectionName) => {
-  const { secret } = req.query;
-  if (secret === AuthKey) {
     await setModel(collectionName).deleteMany({});
     res
       .status(200)
       .json({ message: collectionName + " Collection cleared successfully" });
-  }
-  else {
-    res.status(400).json({ error: "You are not authorized" });
-  }
 };
 
 module.exports = { clearCollection };
