b8ee8626d83a443a91df23dad62ae6b600fcd07c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2025-04-11 11:04:38 +0530

    Refactor SearchBox button click handler for improved readability and maintainability

--- client/online-employee-management-system/src/components/SearchBox/search-box.component.tsx
+++ client/online-employee-management-system/src/components/SearchBox/search-box.component.tsx
@@ -48,15 +48,17 @@ export default function SearchBox(props: propsType) {
             required
           />
           <button
-            onClick={(event) =>
-              props.updateSearchData &&
-              props.updateSearchData(
-                (
-                  (event.currentTarget as HTMLButtonElement)
-                    ?.previousSibling as HTMLInputElement
-                ).value
-              )
-            }
+            onClick={(event) => {
+              if (props.updateSearchData) {
+                event.preventDefault();
+                props.updateSearchData(
+                  (
+                    (event.currentTarget as HTMLButtonElement)
+                      ?.previousSibling as HTMLInputElement
+                  ).value
+                );
+              }
+            }}
             type="submit"
             className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
