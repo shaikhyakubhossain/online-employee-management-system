d536cd21afa79d3072bb9fb09376799c5116879b
Samarjit Sahoo <samarjit9203@gmail.com>
2025-04-14 17:27:28 +0530

    Created ENV Example file

--- client/online-employee-management-system/src/components/Profile/MainBody/main-body.component.tsx
+++ client/online-employee-management-system/src/components/Profile/MainBody/main-body.component.tsx
@@ -119,7 +119,7 @@ export default function MainBody(): JSX.Element {
       <div className="mt-5">
         <Button
           customClassName={styles.btn}
-          customTW="w-full bg-white font-medium shadow-md rounded-lg py-3 hover:bg-blue-200"
+          customTW="w-full bg-white font-medium shadow-md rounded-lg py-3 hover:bg-blue-100"
           onClick={handleLogout}
         >
           <div className="flex items-center justify-center">
